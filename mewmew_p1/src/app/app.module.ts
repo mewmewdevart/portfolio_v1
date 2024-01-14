f5668391e0d0f3aab01d275ca9d092dca3ccab8c
mew <mewmewdevart@gmail.com>
2024-01-14 18:37:19 -0300

    scripts(content) : import the material ui and add the best pratices d my index.html and navbar component

--- mewmew_p1/src/app/app.module.ts
+++ mewmew_p1/src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// Material UI Imports
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule, 
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
