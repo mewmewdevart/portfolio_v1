76a23e74ac965a54838e518e3340826b84b2667d
mew <mewmewdevart@gmail.com>
2024-01-26 17:48:42 -0300

    scripts(content) : test in the backend

--- mewmew_backend/index.js
+++ mewmew_backend/index.js
@@ -1,49 +1,47 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const port = 3000;
+//const port = 3000;
 
 // Rota raiz com documentação
 app.get('/', (req, res) => {
 	const documentation = {
-	  message: 'Bem-vindo a API NodeJS da Mewmew',
-	  routes: {
-		menu: '/api/menu',
-		sectionIds: '/api/section-ids',
-		socialIcons: '/api/social-icons',
-		cardsInfo: '/api/cards-info'
-	  }
+		message: 'Bem-vindo a API NodeJS da Mewmew',
+		routes: {
+			menu: '/api/menu',
+			sectionIds: '/api/section-ids',
+			socialIcons: '/api/social-icons',
+			cardsInfo: '/api/cards-info'
+		}
 	};
   
 	const htmlResponse = `
-    <!DOCTYPE html>
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Documentação da API</title>
-      </head>
-      <body>
-        <h1>${documentation.message}</h1>
-        <h2>Rotas Disponíveis:</h2>
-        <ul>
-          <li><strong>Menu:</strong> <a href="${documentation.routes.menu}" target="_blank">${documentation.routes.menu}</a></li>
-          <li><strong>Section IDs:</strong> <a href="${documentation.routes.sectionIds}" target="_blank">${documentation.routes.sectionIds}</a></li>
-          <li><strong>Social Icons:</strong> <a href="${documentation.routes.socialIcons}" target="_blank">${documentation.routes.socialIcons}</a></li>
-          <li><strong>Cards Info:</strong> <a href="${documentation.routes.cardsInfo}" target="_blank">${documentation.routes.cardsInfo}</a></li>
-        </ul>
-      </body>
-    </html>
+	<!DOCTYPE html>
+	<html lang="en">
+	  <head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Documentação da API</title>
+	  </head>
+	  <body>
+		<h1>${documentation.message}</h1>
+		<h2>Rotas Disponíveis:</h2>
+		<ul>
+		  <li><strong>Menu:</strong> <a href="${documentation.routes.menu}" target="_blank">${documentation.routes.menu}</a></li>
+		  <li><strong>Section IDs:</strong> <a href="${documentation.routes.sectionIds}" target="_blank">${documentation.routes.sectionIds}</a></li>
+		  <li><strong>Social Icons:</strong> <a href="${documentation.routes.socialIcons}" target="_blank">${documentation.routes.socialIcons}</a></li>
+		  <li><strong>Cards Info:</strong> <a href="${documentation.routes.cardsInfo}" target="_blank">${documentation.routes.cardsInfo}</a></li>
+		</ul>
+	  </body>
+	</html>
   `;
 
   // Responda com a página HTML
   res.send(htmlResponse);
 });
-  
 
 // Middleware para permitir requisições com JSON
 app.use(express.json());
-
 app.use(cors());
 
 // Rota para obter informações do menu
@@ -58,18 +56,18 @@ app.get('/api/menu', (req, res) => {
 
 app.get('/api/section-ids', (req, res) => {
 	const sectionIds = {
-	  about: 'about',
-	  projects: 'projects',
-	  contact: 'contact'
+		about: 'about',
+		projects: 'projects',
+		contact: 'contact'
 	};
 	res.json(sectionIds);
-  });
+});
 
 // Rota para obter informações dos ícones sociais
 app.get('/api/social-icons', (req, res) => {
 	const socialIcons = [
-		{ name: 'Icone de acesso do Github', iconPath: '../assets/assets/images/icons/icon_github.svg', url: '#' },
-		{ name: 'Icone de acesso do Linkedin', iconPath: '../assets/assets/images/icons/icon_linkedin.svg', url: '#' }
+		{ name: 'Icone de acesso do Github', iconPath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/8c8e693a4671b8d8843263d2cba3831f23673a84/mewmew_frontend/src/assets/assets/images/icons/icon_github.svg', url: '#' },
+		{ name: 'Icone de acesso do Linkedin', iconPath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/8c8e693a4671b8d8843263d2cba3831f23673a84/mewmew_frontend/src/assets/assets/images/icons/icon_linkedin.svg', url: '#' }
 	];
 	res.json(socialIcons);
 });
@@ -78,31 +76,34 @@ app.get('/api/social-icons', (req, res) => {
 app.get('/api/cards-info', (req, res) => {
 	const cardsInfo = [
 		{
-			imagePath: '../assets/assets/images/placeholder.png',
+			imagePath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/main/mewmew_frontend/src/assets/assets/images/placeholder.png',
 			titleProject: 'Card 1',
 			altTitle: 'Alternative text',
 			tagsTitle: ['Angular', 'MaterialUI', 'TypeScript'],
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nibh ac sem aliquam aliquet.',
 			liveUrl: '#',
 			codeUrl: '#'
-		},		{
-			imagePath: '../assets/assets/images/placeholder.png',
+		},
+		{
+			imagePath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/main/mewmew_frontend/src/assets/assets/images/placeholder.png',
 			titleProject: 'Card 1',
 			altTitle: 'Alternative text',
 			tagsTitle: ['Angular', 'MaterialUI', 'TypeScript'],
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nibh ac sem aliquam aliquet.',
 			liveUrl: '#',
 			codeUrl: '#'
-		},		{
-			imagePath: '../assets/assets/images/placeholder.png',
+		},
+		{
+			imagePath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/main/mewmew_frontend/src/assets/assets/images/placeholder.png',
 			titleProject: 'Card 1',
 			altTitle: 'Alternative text',
 			tagsTitle: ['Angular', 'MaterialUI', 'TypeScript'],
 			description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae nibh ac sem aliquam aliquet.',
 			liveUrl: '#',
 			codeUrl: '#'
-		},		{
-			imagePath: '../assets/assets/images/placeholder.png',
+		},
+		{
+			imagePath: 'https://raw.githubusercontent.com/mewmewdevart/portfolio_v1/main/mewmew_frontend/src/assets/assets/images/placeholder.png',
 			titleProject: 'Card 1',
 			altTitle: 'Alternative text',
 			tagsTitle: ['Angular', 'MaterialUI', 'TypeScript'],
@@ -114,7 +115,9 @@ app.get('/api/cards-info', (req, res) => {
 	res.json(cardsInfo);
 });
 
-// Inicie o servidor
+
+/* Inicie o servidor
 app.listen(port, () => {
 	console.log(`Servidor está rodando em http://localhost:${port}`);
 });
+*/
