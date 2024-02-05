18e8b99039aac04b06ba7c866291e60e8d71d164
mew <mewmewdevart@gmail.com>
2024-02-04 21:30:38 -0300

    scripts(content) : update in the scroll options

--- mewmew_frontend/src/app/footer/footer.component.ts
+++ mewmew_frontend/src/app/footer/footer.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommunicationService, SocialItem } from '../communication.service';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'app-footer',
@@ -9,13 +10,22 @@ import { CommunicationService, SocialItem } from '../communication.service';
 export class FooterComponent implements OnInit {
 	socialIcons: SocialItem[] = [];
 	currentYear: number = new Date().getFullYear();
+	isHomePage: boolean = true;
 
 	constructor(
+		private router: Router,
 		private communicationService: CommunicationService
-	 ) {}
+	) {}
 
 	ngOnInit() {
 		this.currentYear = new Date().getFullYear();
 		this.socialIcons = this.communicationService.socialItems;
+
+		const isProjectsPage = this.router.url.includes('more-projects');
+		const isAboutPage = this.router.url.includes('about-me');
+
+		if (isProjectsPage || isAboutPage) {
+			this.isHomePage = false;
+		}
 	}
 }
